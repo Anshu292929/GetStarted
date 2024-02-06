7af1b8c5a720f214158de329392be875b93fef6e
Anshu Asthana <anshuasthana2001@yahoo.com>
2024-02-05 20:39:46 -0600

    Initial Update

---
+++ src/main/java/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int i=10;
+        for (int i1 = 0; i1 < i; i1++) {
+            System.out.println(i);
+        }
+    }
+}
